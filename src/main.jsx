53e0ec5080103c9cdd7943772efb64062dad118f
Rajs1236 <raj22f069@gmail.com>
2025-05-21 00:26:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App(1).jsx'
+import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store(1).js'
